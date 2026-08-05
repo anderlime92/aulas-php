0d4c669b642a191e4af7647fd0865f564b2445ca
anderlime92 <ander29mendonsa@gmail.com>
2026-08-05 08:59:18 -0300

    revisa mistura html com php

---
+++ 08-intercalando-php-html.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revisando PHP intercalado com HTML</title>
+</head>
+
+<body>
+    <h1>Revisando PHP intercalado com HTML</h1>
+    <hr>
+
+    <?php
+    $aluno = "Fulano";
+    const ESCOLA = "Senac Penha";
+
+    echo "<p>O aluno se chama $aluno</p>";
+    echo "<p><p></p>  " . ESCOLA . "</p>";
+    ?>
+
+    <h2>Usando PHP intercalando com HTML</h2>
+    <p><i>(usar o PHP onde precisa)</i></p>
+    <p>O aluno se chama <?= $aluno ?></p>
+    <p>Ele estuda na escola <?= ESCOLA ?></p>
+
+    <hr>
+
+    <h2>Usando o PHP intercalando comandos com HTML</h2>
+
+    <?php $idade = 25; ?> <!-- PHP onde preciso --> 
+
+    <?php 
+    if ($idade >= 18) {
+        echo "<p><b>$aluno</b> é maior de idade</p>"; 
+    } else {
+        echo "<p><i>$aluno</i> é menor de idade</p>";
+    }
+    ?>
+
+    <h3>Resultado:</h3>
+</body>
+
+</html>
