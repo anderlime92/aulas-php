39e79612db84d3f5737f1c15295a990ebc7bacb0
anderlime92 <ander29mendonsa@gmail.com>
2026-08-10 10:40:35 -0300

    Loop foreach para array numerico

--- 10-loops-para-estrutura-de-dados.php
+++ 10-loops-para-estrutura-de-dados.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,43 +9,57 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
+
 <body>
     <div class="container">
         <h1>Loops e dados para estruturas de dados</h1>
         <hr>
 
-    <?php 
-    $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-    ?>  
-    
-    <h2>Usando o loop for para acessar o array</h2>
-    <ol>
-        <?php for ($i=0; $i < count($meses); $i++) { ?>
-            <li> <?= $meses[$i] ?> </li>
-        <?php } ?>
-    </ol>
+        <?php
+        $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        ?>
 
-    <hr>
+        <h2>Usando o loop for para acessar o array</h2>
+        <ol>
+            <?php for ($i = 0; $i < count($meses); $i++) { ?>
+                <li> <?= $meses[$i] ?> </li>
+            <?php } ?>
+        </ol>
 
-    <h2>Usando o loop for para acessar uma matriz(array de arrays)</h2>
-    <?php 
-    $planoDeEstudos = [["JS Avançado", "Node.js", "Next.js"], ["PHP", "Orientação a objetos"], ["Teoria das Cores", "Photoshop com AI", "UX/UI"]];
-    $linhas = count($planoDeEstudos);
-    for ($i=0; $i < $linhas; $i++): // acessa cada linha
-        $colunas = count($planoDeEstudos[$i]);
-        for ($j=0; $j < $colunas; $j++): // acessa cada coluna
-    ?>
-        <p> <?= $planoDeEstudos[$i][$j] ?></p>
-    <?php  
-        endfor; // fim do acesso a cada coluna
-    endfor; // fim do acesso a cada linha
-    ?>
+        <hr>
+
+        <h2>Usando o loop for para acessar uma matriz(array de arrays)</h2>
+        <?php
+        $planoDeEstudos = [["JS Avançado", "Node.js", "Next.js"], ["PHP", "Orientação a objetos"], ["Teoria das Cores", "Photoshop com AI", "UX/UI"]];
+        $linhas = count($planoDeEstudos);
+        for ($i = 0; $i < $linhas; $i++): // acessa cada linha
+            $colunas = count($planoDeEstudos[$i]);
+            for ($j = 0; $j < $colunas; $j++): // acessa cada coluna
+        ?>
+                <p> <?= $planoDeEstudos[$i][$j] ?></p>
+        <?php
+            endfor; // fim do acesso a cada coluna
+        endfor; // fim do acesso a cada linha
+        ?>
+
+        <hr>
+
+        <h2>Usando o loop foreach para arrays</h2>
+        <?php
+        $alunos = ["Thiago", "Adela", "Renan", "Pérola"];
+        foreach ($alunos as $aluno):
+        ?>
+            <p> <?= $aluno ?> </p>
+        <?php
+        endforeach;
+        ?>
 
     </div>
 
-    
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
