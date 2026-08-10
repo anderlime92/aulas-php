38c9b4734b21e82b691837f2c3fc8f9bdd0e7c06
anderlime92 <ander29mendonsa@gmail.com>
2026-08-10 09:39:45 -0300

    loops div

--- 09-loops.php
+++ 09-loops.php
@@ -11,59 +11,61 @@
 </head>
 
 <body>
-    <h1>Trabalhando com comandos de repetição</h1>
-    <hr>
+    <div class="container">
+        <h1>Trabalhando com comandos de repetição</h1>
+        <hr>
 
-    <h2>while (enquanto)</h2>
-    <p>Executa ações repetidas vezes <b>enquanto</b> a condição for <b>verdadeira</b>.</p>
+        <h2>while (enquanto)</h2>
+        <p>Executa ações repetidas vezes <b>enquanto</b> a condição for <b>verdadeira</b>.</p>
 
-    <?php
-    $i = 1;
-    while ($i <= 5): // {
+        <?php
+        $i = 1;
+        while ($i <= 5): // {
 
-    ?>
-
-        <p>Paragráfo: <?= $i ?></p>
-
-    <?php
-        $i++;
-    endwhile; // }
-    ?>
-
-    <hr>
-
-    <h2>do/while (faça/enquanto)</h2>
-    <p>Executa ações pelo menos <b>uma vez</b> e, se a condição for verdadeira, continua executando outras vezes.</p>
-
-    <?php
-    $j = 1;
-    do {
-    ?>
-        <div>
-            <h3>Título qualquer....</h3>
-            <p>Bloco <?= $j ?></p>
-        </div>
-    <?php
-        $j++;
-    } while ($j <= 3);
-    ?>
-
-    <hr>
-
-    <h2>for (para)</h2>
-    <p>Executa ações de acordo com uma <b>quantidade determinada de vezes</b>. </p>
-
-    <section>
-        <h3>Conteúdo da seção/FAQ (Perguntas frequentes)</h3>
-        <?php for ($i = 1; $i <= 5; $i++) { // ou : 
         ?>
-            <details>
-                <summary>Pergunta <?= $i ?></summary>
-                <p>Resposta <?= $i ?></p>
-            </details>
-        <?php } // ou endfor; 
+
+            <p>Paragráfo: <?= $i ?></p>
+
+        <?php
+            $i++;
+        endwhile; // }
         ?>
-    </section>
+
+        <hr>
+
+        <h2>do/while (faça/enquanto)</h2>
+        <p>Executa ações pelo menos <b>uma vez</b> e, se a condição for verdadeira, continua executando outras vezes.</p>
+
+        <?php
+        $j = 1;
+        do {
+        ?>
+            <div>
+                <h3>Título qualquer....</h3>
+                <p>Bloco <?= $j ?></p>
+            </div>
+        <?php
+            $j++;
+        } while ($j <= 3);
+        ?>
+
+        <hr>
+
+        <h2>for (para)</h2>
+        <p>Executa ações de acordo com uma <b>quantidade determinada de vezes</b>. </p>
+
+        <section>
+            <h3>Conteúdo da seção/FAQ (Perguntas frequentes)</h3>
+            <?php for ($i = 1; $i <= 5; $i++) { // ou : 
+            ?>
+                <details>
+                    <summary>Pergunta <?= $i ?></summary>
+                    <p>Resposta <?= $i ?></p>
+                </details>
+            <?php } // ou endfor; 
+            ?>
+        </section>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
