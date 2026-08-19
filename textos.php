9cb018c51bdb749ad15f48a2ec86153d12b48536
anderlime92 <ander29mendonsa@gmail.com>
2026-08-19 11:06:27 -0300

    inclusao de recursos php v2

---
+++ textos.php
@@ -0,0 +1,8 @@
+<!-- textos.php -->
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi architecto molestiae amet excepturi adipisci, mollitia accusamus eius? Dicta, doloribus pariatur eligendi, perspiciatis asperiores officiis voluptates debitis temporibus illo esse obcaecati?</p>
+
+<?php include "lista.html"; ?>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit id suscipit sed vel expedita exercitationem iure eaque voluptatum ipsam repudiandae cumque modi libero, voluptate consectetur alias! Alias vero enim ex.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat possimus corrupti veritatis voluptatem ipsum ea exercitationem facere velit hic, blanditiis repudiandae laboriosam! Et eum esse, quaerat vitae repellendus culpa facilis!</p>
